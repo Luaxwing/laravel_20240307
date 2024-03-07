e1d39079f4c3854a8e400ff3370599f858ae24a4
kai <cgk0910@gmail.com>
2024-03-07 14:39:27 +0800

    add time

--- app/Http/Controllers/CatController.php
+++ app/Http/Controllers/CatController.php
@@ -59,11 +59,15 @@ class CatController extends Controller
         $input = $request->except('_token');
         // die();
         // dd($input);
+        $now = now();
+        // dd($now);
 
         DB::table('cats')->insert([
             'name' => $input['name'],
             'mobile' => $input['mobile'],
-            'address' => 999
+            'address' => 999,
+            'created_at' => $now,
+            'updated_at' => $now,
         ]);
         // dd('hello cat store');
         return redirect()->route('cats.index');
